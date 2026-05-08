b19aa21e7a5ae39bd42d2ac33efc3e4afba73e0f
marianaacury <mariana.cury@aluno.senai.br>
2026-05-08 08:30:30 -0300

    Adicionar arquivos de projeto.

---
+++ Aplicatvo teste/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Aplicatvo_teste
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Aplicatvo teste/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Aplicatvo_teste
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

b32938b59987564758c79bd4ae29984e5da332d7
VickyM29 <victoria.c.macedo@aluno.senai.br>
2026-05-08 11:05:21 -0300

    Arrume somente o carossel

--- Aplicatvo teste/MainPage.xaml.cs
+++ Aplicatvo teste/MainPage.xaml.cs
@@ -4,9 +4,51 @@ namespace Aplicatvo_teste
 {
     public partial class MainPage : ContentPage
     {
+        private bool _autoPlay;
+
+        public List<string> Imagens { get; }
+
         public MainPage()
         {
             InitializeComponent();
+
+            Imagens = new List<string>
+        {
+            "carosselp.jpg",
+            "carosselt.jpg"
+        };
+
+            BindingContext = this;
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _autoPlay = true;
+
+            Dispatcher.StartTimer(TimeSpan.FromSeconds(3), () =>
+            {
+                if (!_autoPlay) return false;
+
+                var items = MeuCarrossel.ItemsSource?.Cast<object>().ToList();
+                if (items == null || items.Count == 0) return true;
+
+                var nextIndex = (MeuCarrossel.Position + 1) % items.Count;
+                var nextItem = items[nextIndex];
+
+                // Atualiza o item atual de forma segura na UI
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    MeuCarrossel.CurrentItem = nextItem;
+                });
+
+                return true;
+            });
+        }
+
+        protected override void OnDisappearing()
+        {
+            _autoPlay = false;
+            base.OnDisappearing();
         }
 
         private async void IrAcomodacoes(object sender, EventArgs e)
